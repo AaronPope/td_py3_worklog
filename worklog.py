78b5f554ab049b8f629c4a473a562afc826a243d
unknown <tzemati@gmail.com>
2018-09-11 19:54:54 -0400

    Added outline of TODOs

--- worklog.py
+++ worklog.py
@@ -1,2 +1,17 @@
 while(True):
     pass
+
+# N) New Entry
+#   - Enter task Name
+#   - Enter # of minutes spent working
+#   - Enter additional notes
+#   ## Return to Main Menu
+
+# L) Lookup Previous Entries
+#   D) Find by Date
+#   T) Find by Time Spent (number of minutes)
+#   S) Find by Search (exact) (in Name or Notes)
+#   P) Find by Pattern
+#   B) Go Back || M) Main Menu
+
+# Q) Quit
