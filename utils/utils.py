75a6be5ebe523529cffff87b52335f110c2caf2d
unknown <tzemati@gmail.com>
2018-09-11 20:23:34 -0400

    added utils

---
+++ utils/utils.py
@@ -0,0 +1,18 @@
+# Aaron Pope
+# 05/22/2018
+# Treehouse TechDegree - Python, Unit 2: Secret Messages
+
+import os
+
+EXIT_ARGS = {"Q", "QUIT"}
+
+
+def exit_check(choice):
+    if choice in EXIT_ARGS:
+        exit()
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def print_invalid_entry_message():
+    print("Invalid entry.  Please select again.\n")
