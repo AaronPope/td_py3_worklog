7931f305cf07fd4174fb5cf5b4d1508dd09d9ab8
unknown <tzemati@gmail.com>
2018-09-17 21:28:41 -0400

    moved utils to main directory testing of some menu fuctionality

--- worklog.py
+++ worklog.py
@@ -1,5 +1,26 @@
-while(True):
-    pass
+# Aaron Pope
+# 09/01/2018
+# Treehouse TechDegree - Python, Unit 3: Work Log
+
+import csv
+import datetime
+
+import utils
+import menus
+
+
+if __name__ == '__main__':
+    utils.clear_screen()
+
+    with open("entries.csv") as file:
+        reader = csv.DictReader(file)
+        all_tasks = list(reader)
+
+        print(all_tasks[0])
+
+    # menus.main_menu.get_menu_selection("What would you like to do?", True)
+    # menus.lookup_menu.get_menu_selection("Lookup by...", True)
+
 
 # N) New Entry
 #   - Enter task Name
