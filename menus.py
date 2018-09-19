7718e1ec57f419a5bdf23d2b8fcb826cf420bc59
unknown <tzemati@gmail.com>
2018-09-18 21:35:22 -0400

    added appropriate navigation options to each menu

--- menus.py
+++ menus.py
@@ -1,15 +1,17 @@
 from menu import Menu
 
 main_menu = Menu({
-        "1": "Add New Entry",
-        "2": "Lookup Previous Entries"
+        "N": "Add New Entry",
+        "L": "Lookup Previous Entries",
+        "Q": "Quit"
 })
     
 lookup_menu = Menu({
-        "1": "Date",
-        "2": "Time Spent",
-        "3": "Exact Search",
-        "4": "Pattern Search"
+        "D": "Date",
+        "T": "Time Spent",
+        "S": "Exact Search",
+        "P": "Pattern Search",
+        "B": "Back"
 })
 
 print(main_menu)
