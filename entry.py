fafcc8746fc98a774ed577fb0b8bbe6790b6ee9b
unknown <tzemati@gmail.com>
2018-09-25 20:55:27 -0400

    Added some docstrings Fixed a line length issue

--- entry.py
+++ entry.py
@@ -2,6 +2,7 @@
 # 09/01/2018
 # Treehouse TechDegree - Python, Unit 3: Work Log
 
+
 import datetime
 import re
 
@@ -9,7 +10,15 @@ from utils import validate_date_format
 from utils import validate_positive_int
 
 class Entry:
+    """ An object-oriented representation of a Work Log entry """
     def __init__(self, date, name, minutes, note):
+        """Initialize a new instance of Entry
+
+        date: calendar date on which work was performed
+        name: name/title of work log entry
+        minutes: time spent on work (expects integer)
+        note: extra information about the work log entry
+        """
         validate_date_format(date)
         validate_positive_int(minutes)
         
@@ -26,10 +35,13 @@ class Entry:
 
     @classmethod
     def create(cls):
+        """ Returns a new instance of Entry, based on user input """
         cancel_kw = "CANCEL"
         print("*** New Worklog Entry ***")
-        print(f"[Type {cancel_kw} (case-sensitve) for any field to cancel new entry.]\n")
+        print(f"[Type {cancel_kw} (case-sensitve) "
+              + "for any field to cancel new entry.]\n")
 
+        # Get user input for Date
         date = str(input(f"Enter date in YYYY-MM-DD format\n"
                     + "(or press ENTER for {datetime.date.today()})\n>>> ")
                     or datetime.date.today())
@@ -44,10 +56,12 @@ class Entry:
             else:
                 break
         
+        # Get user input for task Name
         name = input("\nEnter task name\n>>> ")
         if name == cancel_kw:
             return None
         
+        # Get user input for task Minutes
         minutes = input("\nEnter number of minutes\n>>> ")
         if minutes == cancel_kw:
             return None
@@ -59,7 +73,10 @@ class Entry:
                 minutes = input(">>> ")
             else:
                 break
+                
+        # Get user input for task Note
         note = input("\nEnter a note for this entry (optional)\n>>> ")
         if note == cancel_kw:
             return None
+        
         return cls(date, name, minutes, note)
