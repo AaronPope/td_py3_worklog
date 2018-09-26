b37d2619136db4da970bcfaef05335343f0bd4ac
unknown <tzemati@gmail.com>
2018-09-25 20:54:01 -0400

    finished merge

--- app.py
+++ app.py
@@ -2,15 +2,14 @@
 # 09/01/2018
 # Treehouse TechDegree - Python, Unit 3: Work Log
 
-from entry import Entry
+""" Serves as the launch point for the Work Log tool """
+
 from menus import lookup_menu
 from menus import main_menu
 from utils import clear_screen
 from utils import continue_prompt
 from utils import exit_check
-from worklog import Worklog
-
-        
+from worklog import Worklog        
 
 if __name__ == '__main__':
     clear_screen()
